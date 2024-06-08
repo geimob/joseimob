cb2006944491cd3cacf7ea9befb4d738201e8a3b
José Henrique <josehenrique.tropalde@gmail.com>
2024-06-08 10:29:22 -0300

    feat: add nextjs ambient

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 text-center">
+      <h1>Template GEIMOB Regular</h1>
+      <h2>Bem vindo</h2>
+      <h3>*Seu conteúdo vai aqui*</h3>
+    </main>
+  );
+}
