69c51513f1f4bba0d925eb2119ab5cc0ebca78ca
José Henrique <josehenrique.tropalde@gmail.com>
2024-06-09 19:32:24 -0300

    feat: add github actions

---
+++ .scripts/sync.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+REPOS=(
+    "https://github.com/geimob/joseimob.git"
+)
+
+TEMP_DIR=$(mktemp -d)
+
+update_repo() {
+    REPO_URL=$1
+    REPO_NAME=$(basename $REPO_URL .git)
+    
+    git clone $REPO_URL $TEMP_DIR/$REPO_NAME
+    
+    cd $TEMP_DIR/$REPO_NAME
+    
+    git remote add template https://github.com/geimob/geimob-regular.git
+    
+    git fetch template
+    
+    git merge template/main -m "Update from template"
+    
+    git push origin main
+    
+    cd -
+}
+
+for REPO in "${REPOS[@]}"; do
+    update_repo $REPO
+done
+
+rm -rf $TEMP_DIR
