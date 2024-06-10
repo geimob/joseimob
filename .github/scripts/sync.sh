ca861e30ee1db0a5cb79e43b0247e820d9e414cc
José Henrique <josehenrique.tropalde@gmail.com>
2024-06-09 21:21:54 -0300

    feat: update github action

--- .github/scripts/sync.sh
+++ .github/scripts/sync.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Define your GitHub Personal Access Token
-GITHUB_TOKEN="YOUR_PERSONAL_ACCESS_TOKEN"
-
 REPOS=(
     "https://github.com/geimob/joseimob.git"
 )
@@ -12,13 +9,10 @@ TEMP_DIR=$(mktemp -d)
 update_repo() {
     REPO_URL=$1
     REPO_NAME=$(basename $REPO_URL .git)
-
+    
     git clone $REPO_URL $TEMP_DIR/$REPO_NAME
     
     cd $TEMP_DIR/$REPO_NAME
-    
-    git config credential.helper store
-    echo "https://$GITHUB_TOKEN:x-oauth-basic" > ~/.git-credentials
     
     git remote add template https://github.com/geimob/geimob-regular.git
     
@@ -35,5 +29,4 @@ for REPO in "${REPOS[@]}"; do
     update_repo $REPO
 done
 
-# Clean up temporary directory
 rm -rf $TEMP_DIR
